1e23373152ca97ef96e116212799c425ff37ea7a
Xuchen.Xia <xiaxucheng@126.com>
2016-03-25 13:48:28 +0800

    This is a Demo of Navigation

---
+++ TempNavigationApp(UWP)/TempNavigationApp/TempNavigationApp/bin/Debug/AppX/js/default.js
@@ -0,0 +1,60 @@
+﻿// For an introduction to the Navigation template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232506
+(function () {
+    "use strict";
+
+    var activation = Windows.ApplicationModel.Activation;
+    var app = WinJS.Application;
+    var nav = WinJS.Navigation;
+    var sched = WinJS.Utilities.Scheduler;
+    var ui = WinJS.UI;
+
+    app.addEventListener("activated", function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                // TODO: This application has been newly launched. Initialize
+                // your application here.
+                var currentview = Windows.UI.Core.SystemNavigationManager.getForCurrentView();
+                currentview.appViewBackButtonVisibility = Windows.UI.Core.AppViewBackButtonVisibility.visible;
+                currentview.onbackrequested = onBackRequested;
+                function onBackRequested(eventArgs) {
+                    if (WinJS.Navigation.canGoBack) {
+                        WinJS.Navigation.back(1).done(function (successInformation) {
+                            /*this is the success function*/
+                        }, function (error) {
+                            /*this is the error function*/
+                        });
+                    }
+                }
+            } else {
+                // TODO: This application was suspended and then terminated.
+                // To create a smooth user experience, restore application state here so that it looks like the app never stopped running.
+            }
+
+            nav.history = app.sessionState.history || {};
+            nav.history.current.initialPlaceholder = true;
+
+            // Optimize the load of the application and while the splash screen is shown, execute high priority scheduled work.
+            ui.disableAnimations();
+            var p = ui.processAll().then(function () {
+                return nav.navigate(nav.location || Application.navigator.home, nav.state);
+            }).then(function () {
+                return sched.requestDrain(sched.Priority.aboveNormal + 1);
+            }).then(function () {
+                ui.enableAnimations();
+            });
+
+            args.setPromise(p);
+        }
+    });
+
+    app.oncheckpoint = function (args) {
+        // TODO: This application is about to be suspended. Save any state
+        // that needs to persist across suspensions here. If you need to 
+        // complete an asynchronous operation before your application is 
+        // suspended, call args.setPromise().
+        app.sessionState.history = nav.history;
+    };
+
+    app.start();
+})();
---
+++ TempNavigationApp(UWP)/TempNavigationApp/TempNavigationApp/pages/home/home.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/home/home.html", {
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+            // TODO: Initialize the page here.
+            document.getElementById("myBtn").onclick = function () {
+                WinJS.Navigation.navigate("/pages/pageOne/pageOne.html");
+            }
+        }
+    });
+})();
