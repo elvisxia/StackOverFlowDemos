e22ab77c7e8e119d90743003530de9ef26fda769
Xuchen.Xia <xiaxucheng@126.com>
2016-04-08 13:47:43 +0800

    Scroll Position Sample

---
+++ HorizontalScrollPositionSamples/UWPSamples/ProblemSample/ProblemSample/js/default.js
@@ -0,0 +1,34 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+	"use strict";
+
+	var app = WinJS.Application;
+	var activation = Windows.ApplicationModel.Activation;
+
+	app.onactivated = function (args) {
+		if (args.detail.kind === activation.ActivationKind.launch) {
+			if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+			    // TODO: This application has been newly launched. Initialize your application here.
+			    var myDiv = document.getElementById("myDiv");
+			    var yourDiv = document.getElementById("yourDiv");
+			    window.addEventListener("scroll", function (evt) {
+			        yourDiv.innerText = "The scrollLeft Value is: " + evt.srcElement.scrollLeft;//scrollLeft stays 0;
+			        //yourDiv.style.marginLeft = evt.srcElement.scrollLeft + "px";
+			    }, true);
+			} else {
+				// TODO: This application was suspended and then terminated.
+				// To create a smooth user experience, restore application state here so that it looks like the app never stopped running.
+			}
+			args.setPromise(WinJS.UI.processAll());
+		}
+	};
+
+	app.oncheckpoint = function (args) {
+		// TODO: This application is about to be suspended. Save any state that needs to persist across suspensions here.
+		// You might use the WinJS.Application.sessionState object, which is automatically saved and restored across suspension.
+		// If you need to complete an asynchronous operation before your application is suspended, call args.setPromise().
+	};
+
+	app.start();
+})();
---
+++ HorizontalScrollPositionSamples/WebSample/WebApp/js/default.js
@@ -0,0 +1,15 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+    "use strict";
+
+                var myDiv = document.getElementById("myDiv");
+                var yourDiv = document.getElementById("yourDiv");
+                window.addEventListener("scroll", function (evt) {
+                    yourDiv.innerText = evt.srcElement.body.scrollLeft
+                }, true);
+
+                myDiv.onscroll = function (evt) {
+                    //yourDiv.innerText=myDiv.scrollLeft;
+                }
+})();
